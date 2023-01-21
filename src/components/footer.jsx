a57982ca38c679cd61334c80d3069edf6b09f903
Adi <abhan03@bu.edu>
2023-01-21 07:29:22 -0500

    first commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,30 +1,47 @@
+import "../index.css";
 //-------------------------------------------------------------------------------------------------
 // CSS INLINE STYLING USING TAILWIND
 
 const footer_background =
-  "text-center h-5  p-2 bg-zinc-900  shadow md:flex md:items-center  md:p-2 dark:bg-zinc-900 ";
+  " bounce2 round-full rounded-lg border border-white content-center	 p-2 bg-zinc-300  shadow md:flex md:items-center md:justify-between md:p-2 dark:bg-zinc-900";
 const footer_text =
-  "text-center mx-auto justify-center scale-7 italic text-sm text-right text-gray-400 sm:text-center ";
-const footer_hover = "hover:text-bl-200 hover:underline";
+  "bounce2  text-sm text-right text-gray-400 sm:text-center dark:text-gray-400";
+const footer_hover =
+  "bounce2  hover:text-bl-500 hover:underline hover:scale-110";
+const background = " bg-zinc-800 p-2 ";
 
 //-------------------------------------------------------------------------------------------------
 
 function Footers() {
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
   return (
-    <div className="bg-black justify-center scale-150">
-      <footer className={footer_background}>
-        <span className={footer_text}>
-          All Rights Reserved | Created by Adi Bhan with ❤️ from GitHub ➡️{" "}
-          <a
-            href="https://github.com/AdiBhan/Portfolio"
-            className={footer_hover}
-          >
-            Click to view source
-          </a>
-          .
-        </span>
-      </footer>
-    </div>
+    <>
+      <div className={background}>
+        <footer class={footer_background}>
+          <div className=" flex flex-col items-center">
+            <button
+              onClick={topFunction}
+              type="button"
+              class="bounce2 btn btn-warning content-center"
+            ></button>
+          </div>
+          <p></p>
+          <span class={footer_text}>
+            All Rights Reserved | Created by Adi Bhan with ❤️ from GitHub ➡️{" "}
+            <a
+              href="https://github.com/AdiBhan/UI-GroceryApp"
+              class={footer_hover}
+            >
+              Click to view source
+            </a>
+            .
+          </span>
+        </footer>{" "}
+      </div>
+    </>
   );
 }
 export default Footers;
